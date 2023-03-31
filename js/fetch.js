e82d5e1caacf0bc53e43419167694fc25045cb3c
Arykaanisap <arykaanisap22@gmail.com>
2023-03-31 15:43:01 +0700

    bbbb

--- js/fetch.js
+++ js/fetch.js
@@ -1,5 +1,5 @@
 import { get } from "https://bukulapak.github.io/api/process.js"; 
-let urlAPI = "https://dashboard.heroku.com/apps/aryka";
+let urlAPI = "https://aryka.herokuapp.com/presensi";
 get(urlAPI,isiTablePresensi);
 function isiTablePresensi(results){
     console.log(results);

fe4ca10d3aa8e07e55fad041cf125f74c1de7a79
Arykaanisap <arykaanisap22@gmail.com>
2023-03-31 17:17:54 +0700

    ttt

--- js/controller/get.js
+++ js/controller/get.js
@@ -12,9 +12,9 @@ function isiRow(value) {
             .replace("#LOKASI#", value.location)
             .replace("#STATUS#", value.checkin)
             .replace("#HARIKERJA#", value.biodata.hari_kerja ? value.biodata.hari_kerja:"HARIKERJA")
-            .replace("#JAMKERJA#", value.biodata.jamkerja ? value.biodata.jam_kerja[0].durasi : "JAMKERJA")
-            .replace("#JAM MASUK#", value.biodata.jamkerja ? value.biodata.jam_masuk[0].durasi : "JAMMASUK" )
-            replace("#JAM KELUAR#", value.biodata.jamkerja ? value.biodata.jam_keluar[0].durasi : "JAMKELUAR" )
+            .replace("#JAMKERJA#", value.biodata.jam_kerja ? value.biodata.jam_kerja[0].durasi : "JAMKERJA")
+            .replace("#JAM MASUK#", value.biodata.jam_kerja ? value.biodata.jam_kerja[0].jam_masuk : "JAMMASUK" )
+            replace("#JAM KELUAR#", value.biodata.jam_kerja ? value.biodata.jam_kerja[0].jam_keluar : "JAMKELUAR" )
             .replace("#WARNA#", getRandomColor())
             .replace(/#WARNALOGO#/g, getRandomColorName());
     addInner("iniTabel", content);

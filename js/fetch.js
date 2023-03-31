48553b4cea8a4ab2ba1129f651e294ab850a27e3
Arykaanisap <arykaanisap22@gmail.com>
2023-03-31 15:49:02 +0700

    ccc

--- js/fetch.js
+++ js/fetch.js
@@ -1,6 +1,11 @@
-import { get } from "https://bukulapak.github.io/api/process.js"; 
+import { get } from "https://jscroot.github.io/api/croot.js";
+import { setInner } from "https://jscroot.github.io/element/croot.js";
 let urlAPI = "https://aryka.herokuapp.com/presensi";
 get(urlAPI,isiTablePresensi);
 function isiTablePresensi(results){
     console.log(results);
+    results.forEach(isiRow);
+}
+function isiRow(value){
+    console.log(value)
 }

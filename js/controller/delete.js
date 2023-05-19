a72c3b9cfef66d0587f187cf57e3f20fd911cf9b
ArykaAnisaP <arykaanisap22@gmail.com>
2023-05-19 16:28:53 +0700

    tg10

--- js/controller/delete.js
+++ js/controller/delete.js
@@ -1,6 +1,6 @@
 function deleteData(IDHAPUS) {
     var presensiId = IDHAPUS;
-    var target_url = "https://ws-ulbi.herokuapp.com/delete/" + presensiId;
+    var target_url = "https://aryka.herokuapp.com/delete/" + presensiId;
 
     var requestOptions = {
         method: 'DELETE',
